43ba15b07ac55313875a8ea673939d5e622bbd58
Amy Chew <amychew@amys-mbp.attlocal.net>
2017-11-01 17:54:44 -0700

    Completed homework 8

--- hw08/js/main.js
+++ hw08/js/main.js
@@ -2,21 +2,43 @@
 // PART I: Using a while loop, print the message provided 100 times
 //------------------------------------------------------------------
 
+// Defining a function here.
 const name = 'John'
 
+// Function created below as an easy way to add list items to the DOM.
 // Creates a list element and appends it to the unordered list in part1
 const appendListElement = (text) => {
+// creates an empty list item container.
   const li = document.createElement('li')
+// creates text node in the document  that contains the string stored in "text"
+// variable. This is the same as the message that was passed into the function.
+// "text" parameter is a variable.
   const textNode = document.createTextNode(text)
+// attaches text node to the list item. adds the text in your list item container
   li.appendChild(textNode)
+// find the element that has the element with id part 1 and appends the list tiem.
   document.querySelector('#part1').appendChild(li)
 }
 
-appendListElement('Welcome, ' + name + '! Make yourself comfortable.')
+//beginning our while loop syntax
+let counter = 0
+while (counter < 100) {
+  // make sure appendListElement definition precedes calling the function. you
+  // have to define it first before calling it!
+  appendListElement('Hey Dude Make me a Krabby Patty')
+  ++counter
+}
+
+//Using a function here.
+//appendListElement('Welcome, ' + name + '! Make yourself comfortable.')
+//appendListElement('Hey Dude')
+
+
 
 //------------------------------------------------------------------
 // PART II: Using a for loop, write out each person’s avatar, name, and score, based on the people array
 //------------------------------------------------------------------
+// An array of objects.
 //The data:
 var people = [
     { name: 'Jane', pic: 'http://knight.gamebanana.com/img/ico/sprays/patrick_star_preview_2.png', score: 300 },
@@ -31,30 +53,37 @@ var people = [
 // use a for loop to DRY up the printNames function
 // it should loop over the people array and append
 // the users avatar pic, a greeting, and their score to the HTML
-const printNames = () => {
+const printNames = (counter) => {
   // Message 1
+  // create the image
   const img1 = document.createElement('img')
-  img1.src = people[0].pic
+  // set the source to whichever person
+  img1.src = people[counter].pic
+  //this shrinks the photo
   img1.classList.add('avatar')
 
   const paragraph1 = document.createElement('p')
-  const text1 = document.createTextNode('Welcome, ' + people[0].name + '! Your score is: ' + people[0].score)
+  const text1 = document.createTextNode('Welcome, ' + people[counter].name + '! Your score is: ' + people[counter].score)
   paragraph1.appendChild(text1)
 
   document.querySelector('#part2').appendChild(img1)
   document.querySelector('#part2').appendChild(paragraph1)
 
-  // Message 2
+// Question: When I comment out message 2, all my text for part two disappears. Why? 
+// Message 2
   const img2 = document.createElement('img')
-  img2.src = people[1].pic
+  img2.src = people[2].pic
   img2.classList.add('avatar')
 
   const paragraph2 = document.createElement('p')
-  const text2 = document.createTextNode('Welcome, ' + people[1].name + '! Your score is: ' + people[1].score)
+  const text2 = document.createTextNode('Welcome, ' + people[2].name + '! Your score is: ' + people[2].score)
   paragraph2.appendChild(text2)
 
   document.querySelector('#part2').appendChild(img2)
   document.querySelector('#part2').appendChild(paragraph2)
 }
 
-printNames()
+//ARRAY + LOOP teamwork
+  for (let i = 0; i < 6; i++) {
+    printNames(i)
+  }
