7407b95963f7c24fcfe0089ed4c27b1822092f62
Amy Chew <amychew@amys-mbp.attlocal.net>
2017-11-22 23:30:03 -0800

    Completed homework 10

--- hw10/js/main.js
+++ hw10/js/main.js
@@ -1,15 +1,47 @@
 const apiKey = 'MW9S-E7SL-26DU-VV8V'
 
+// Building the url to ask the question you want to ask. Below give me a list
+// of all the bart stations.
 const makeStationList = () => {
     const url = 'https://api.bart.gov/api/stn.aspx?key=' + apiKey +
                     '&cmd=stns&json=y'
+// go out and get data associated with address above.
     fetch(url)
         .then((response) => {
             return response.json()
         })
         .then((json) => {
+            // Do something with the data you have retrieved.
             json = json.root
-            console.log(json)
+
+          /* FOR LOOP
+            const stations = json.stations.station
+            for(let i = 0; i < stations.length; i++) {
+            // json station.station structure can be seen in the XML document from BART API
+            console.log(stations[i].name)
+          }*/
+
+          const stations = json.stations.station
+          stations.forEach((station) => {
+              console.log(station.name)
+              // 1. Create the element you want to add to the DOM.
+              //   In this case, create an empty option element.
+              //   We need to append a child each time we loop through the data.
+              const option = document.createElement("option")
+              // 2. Assign data to some part of the element. Properties to elements.
+              //    In this case, we are setting option's innerHTML to the station name.
+              //    Also, setting value to station abbreviation.
+              option.innerHTML = station.name
+              option.value = station.abbr
+              // 3. Append the newly created element to the DOM somewhere.
+              document.getElementById('station_list').appendChild(option)
+          })
+
+
+            // How do I use the data returned from BART to populate my drop down
+            // menu?
+
+
 
             // PART III.A.: Use a loop to populate the select menu with *ALL*
             // of the stations that are returned from the BART data feed:
@@ -23,11 +55,85 @@ const makeStationList = () => {
         })
 }
 
+makeStationList()
+
+
 const getArrivalTimes = () => {
+    // Find id station list and store in variable called station list.
     const stationList = document.getElementById('station_list')
-    // PART III.B.1: The bartStationCode should read from the list and query
-    // for the corresponding station
-    const bartStationCode = 'DBRK'
+
+    // get the station list's selected value
+    const bartStationCode = stationList.value
+
+    // print it to the screen
+    console.log(bartStationCode)
+
+    // using the in bartStationCode
+    console.log('Selected Station Code:', bartStationCode)
+    let url = 'https://api.bart.gov/api/etd.aspx?key=MW9S-E7SL-26DU-VV8V&cmd=etd&orig=' +
+                bartStationCode + '&json=y'
+    fetch(url)
+        .then((response) => {
+            return response.json()
+        })
+        .then((json) => {
+            json = json.root
+
+            // 1. Clear out existing div.
+            document.getElementById('results').innerHTML = ''
+
+            // 2. Add header that shows the selected station names.
+            const header = document.createElement("h2")
+            header.innerHTML =  json.station[0].name
+            document.getElementById('results').appendChild(header)
+
+            // 3. Log the train destination. Ex: "Warm Springs, Milbrae"
+
+            // Print name of train to p tag and append to dom.
+            json.station[0].etd.forEach((line) => {
+              console.log(line)
+              const trainLine  = document.createElement("h3")
+              trainLine.innerHTML = line.destination
+
+              // Also add the platform
+              trainLine.innerHTML +=  ': Platform # ' + line.estimate[0].platform
+
+              // Also add the train direction
+              trainLine.innerHTML += ' (' + line.estimate[0].direction + ')'
+
+              // now append to the DOM
+              document.getElementById('results').appendChild(trainLine)
+
+              // now add color of the train
+              const square  = document.createElement("span")
+              square.style.background = line.estimate[0].hexcolor
+              square.classList.add('train-square')
+              document.getElementById('results').appendChild(square)
+
+            //4. Log all of the estimates for each train line.
+              const departureTimes = []
+                line.estimate.forEach((estimate) => {
+                  departureTimes.push(estimate.minutes)
+                    console.log(estimate)
+                })
+
+
+              // encapsulate each time estimate in a span tag. Span is like
+              // an empty box that holds information.
+              const departureTime  = document.createElement("span")
+              departureTime.innerHTML =  departureTimes.join(', ')
+              departureTime.innerHTML += ' minutes '
+              document.getElementById('results').appendChild(departureTime)
+
+          })
+
+        })
+
+    }
+
+
+
+    /*
     console.log('Selected Station Code:', bartStationCode)
     let url = 'https://api.bart.gov/api/etd.aspx?key=' + apiKey + '&cmd=etd' +
                 '&orig=' + bartStationCode + '&json=y'
@@ -38,6 +144,7 @@ const getArrivalTimes = () => {
         .then((json) => {
             json = json.root
             console.log(json)
+
             const results = document.getElementById('results')
             results.innerHTML = ''
             json.station = json.station[0]
@@ -73,3 +180,5 @@ const getArrivalTimes = () => {
 }
 
 makeStationList()
+
+*/
